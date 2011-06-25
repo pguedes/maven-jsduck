3a0a3408e1269d298ff76d0cd52cdb05e4180e6a
wscheele <wscheele@sf-laptop001.sf.local>
2011-06-25 14:55:04 +0200

    amend rdiscount doc to reflect usage of java markdownj

--- maven-jsduck/src/main/resources/rdiscount.rb
+++ maven-jsduck/src/main/resources/rdiscount.rb
@@ -2,7 +2,7 @@ include Java
 
 # fake RDiscount API to satisfy RDiscount dependency of jsduck.
 # RDiscount uses C code by loading a .so file, which won't work in jruby and is not portable.
-# replaces RDiscount with a pure ruby Markdown formatter.
+# replaces RDiscount with a pure java Markdown formatter (markdownj).
 class RDiscount
   def to_html()
     @markdown.markdown(@text)
