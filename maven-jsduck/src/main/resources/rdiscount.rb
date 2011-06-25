f0753f80bbf191cf8893b5e881787bd72707a270
wscheele <wscheele@sf-laptop001.sf.local>
2011-06-25 15:08:22 +0200

    move to java markdown processor

--- maven-jsduck/src/main/resources/rdiscount.rb
+++ maven-jsduck/src/main/resources/rdiscount.rb
@@ -1,15 +1,16 @@
+include Java
+
 # fake RDiscount API to satisfy RDiscount dependency of jsduck.
 # RDiscount uses C code by loading a .so file, which won't work in jruby and is not portable.
-# replaces RDiscount with a pure ruby Markdown formatter.
+# replaces RDiscount with a pure java Markdown formatter (markdownj).
 class RDiscount
   def to_html()
-    @maruku.to_html_document
+    @markdown.markdown(@text)
   end
 
   def initialize(text)
-    require 'maruku'
     @text  = text
-    @maruku = Maruku.new(@text)
+    @markdown = com.petebevin.markdown.MarkdownProcessor.new()
   end
 
 end
